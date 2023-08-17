33447525a2f99dcd9a014d69dc5eb4cebc115ac1
bulricht <108867473+bulricht@users.noreply.github.com>
2023-08-17 20:50:25 +0200

    feat: address pr suggestions

--- py_data_rules/data_model.py
+++ py_data_rules/data_model.py
@@ -3,26 +3,20 @@ from .table import Table
 
 class DataModel:
     def __init__(self, description: dict):
-        for k, v in description.items():
-            assert "path" in v.keys(), f'no "path" defined for alias "{k}"'
-            assert "reader" in v.keys(), f'no "reader" defined for alias "{k}"'
-            assert "schema" in v.keys(), f'no "schema" defined for alias "{k}"'
+        required_keys = {"path", "reader", "schema"}
+        for alias, descr in description.items():
+            missing_keys = required_keys - descr.keys()
+            assert not missing_keys, (
+                f"missing key(s) {missing_keys} in description for alias "
+                f"{alias}"
+            )
         self.description: dict = description
         self.tables: dict = {}
         self._generate_tables()
 
     def _generate_tables(self):
-        for k, v in self.description.items():
-            self.tables.update(
-                {
-                    k: Table(
-                        path=v["path"],
-                        reader=v["reader"],
-                        schema=v["schema"],
-                        alias=k,
-                    )
-                }
-            )
+        for alias, descr in self.description.items():
+            self.tables.update({alias: Table(**descr, alias=alias)})
 
     def __getitem__(self, alias: str) -> Table:
         return self.tables[alias]
