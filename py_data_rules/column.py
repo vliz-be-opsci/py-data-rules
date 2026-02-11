634d599fb076bd3dc2b0a24478d6d7e971ab3564
bulricht <108867473+bulricht@users.noreply.github.com>
2026-02-11 10:59:19 +0100

    refactor and attempt to pass cicd

--- py_data_rules/column.py
+++ py_data_rules/column.py
@@ -4,21 +4,10 @@ from .data_type import DataType, XSDString
 class Column:
     def __init__(
         self,
-        label,
-        data_type: DataType = XSDString(),
-        nullable=False,
-        trim=None,
-    ):
-        self.label = label
-        self.data_type = data_type
-        self.nullable = nullable
-        self.trim = trim
-        assert isinstance(
-            self.data_type, DataType
-        ), "data_type must be an instance of DataType"
-        if self.trim:
-            assert trim in (
-                "leading",
-                "trailing",
-                "both",
-            )  # TODO: enum -> e.g. Trimming.BOTH
+        label: str,
+        data_type: DataType | None = None,
+        nullable: bool | None = None,
+    ) -> None:
+        self.label: str = label
+        self.data_type: DataType = data_type or XSDString()
+        self.nullable: bool = nullable or False
--- tests/test_column.py
+++ tests/test_column.py
@@ -7,8 +7,7 @@ from py_data_rules.data_type import XSDFloat
 class TestColumn(TestCase):
     def test_init(self):
         dt = XSDFloat()
-        c = Column("A", data_type=dt, nullable=True, trim="leading")
+        c = Column("A", data_type=dt, nullable=True)
         self.assertEqual(c.label, "A")
         self.assertEqual(c.data_type, dt)
         self.assertEqual(c.nullable, True)
-        self.assertEqual(c.trim, "leading")
